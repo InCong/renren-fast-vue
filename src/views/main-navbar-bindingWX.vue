4214d819d91e7fb18964dbe60f0c3e638b408cca
incong <inconghuang@gmail.com>
2019-06-24 23:46:11 +0800

    使用websocket来接受后端传来的消息

--- src/views/main-navbar-bindingWX.vue
+++ src/views/main-navbar-bindingWX.vue
@@ -4,7 +4,9 @@
     id="dialog"
     :visible.sync="visible"
     :append-to-body="true"
+    :show-close="false"
     width="480px"
+    @close="closeDialog"
     center>
     <span slot="title"><h1 id = "title">绑定微信</h1></span>
     <div class="marginCenter">
@@ -22,12 +24,67 @@
         qrCodeUrl: ''
       }
     },
+    created () { // 页面创建生命周期函数
+      // this.initWebSocket()
+    },
+    destroyed: function () { // 离开页面生命周期函数
+      // console.log('关闭了websocket')
+      // this.websocketclose()
+    },
     computed: {},
     methods: {
       // 初始化
       init (url) {
         this.visible = true
         this.qrCodeUrl = url
+        this.initWebSocket()
+      },
+      closeDialog () {
+        this.over()
+      },
+      initWebSocket: function () {
+        // WebSocket与普通的请求所用协议有所不同，ws等同于http，wss等同于https
+        let websock = new WebSocket('ws://127.0.0.1:80/renren-fast/websocket/subscribe')
+        websock.onopen = this.websocketonopen
+        websock.onerror = this.websocketonerror
+        websock.onmessage = this.websocketonmessage
+        websock.onclose = this.websocketclose
+        // 组件看不见时调用，中断websocket链接
+        this.over = () => {
+          websock.close()
+        }
+      },
+      websocketonopen: function () {
+        console.log('WebSocket连接成功')
+      },
+      websocketonerror: function (e) {
+        console.log('WebSocket连接发生错误')
+      },
+      websocketonmessage: function (e) {
+        console.log(e.data)
+        if (e.data === 'subscribe_success') {
+          this.visible = false
+          this.$message({
+            message: '已关注公众号，绑定成功！',
+            type: 'sucess',
+            duration: 3000
+          })
+        } else if (e.data === 'subscribe_success') {
+          this.$message({
+            message: '绑定失败！请尝试取消关注再重新关注，如果还没成功，请联系管理员！！！',
+            type: 'error',
+            duration: 4500
+          })
+        } else {
+          this.$message({
+            message: '绑定失败！未知情况，请联系管理员！！！',
+            type: 'error',
+            duration: 4500
+          })
+        }
+      },
+      websocketclose: function (e) {
+        console.log('WebSocket连接关闭')
       }
     }
   }
