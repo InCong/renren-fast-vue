cc29ad7c6ea5669ecf68f31e2d3951f8267f4fca
InCong <371605615@qq.com>
2019-07-09 17:25:41 +0800

    图片上传完成，视频上传待完善

--- src/views/modules/business/teacher-multimedia-add-or-delete.vue
+++ src/views/modules/business/teacher-multimedia-add-or-delete.vue
@@ -6,58 +6,69 @@
     @close="closeDialog">
     <el-tabs v-model="activeName" @tab-click="handleTabClick" type="border-card" v-loading="dataListLoading">
       <el-tab-pane label="图片上传" name="pictureUpload">
-        <el-upload
-          ref="upload"
-          :action="url"
-          multiple
-          :limit="5"
-          list-type="picture-card"
-          :file-list="pictureFileList"
-          :on-preview="handlePictureCardPreview"
-          :on-remove="handlePictureRemove"
-          :on-success="handlePictureSuccess"
-          :on-error="handlePictureError"
-          :on-exceed="handlePictureExceed"
-          :before-upload="beforePictureUpload">
-          <i class="el-icon-plus"></i>
-        </el-upload>
-        <el-dialog :visible.sync="previewDialogVisible" append-to-body>
-          <img width="100%" :src="previewDialogImageUrl" alt="">
-        </el-dialog>
-        <el-table
-          :data="pictureFileList"
-          border
-          v-loading="dataListLoading"
-          style="width: 100%;">
-          <el-table-column
-            type="selection"
-            header-align="center"
-            align="center"
-            width="50">
-          </el-table-column>
-          <el-table-column
-            prop="id"
-            header-align="center"
-            align="center"
-            width="80"
-            label="ID">
-          </el-table-column>
-          <el-table-column
-            prop="name"
-            header-align="center"
-            align="center"
-            label="图片名称">
-          </el-table-column>
-          <el-table-column
-            prop="createTime"
-            header-align="center"
-            align="center"
-            label="上传时间">
-          </el-table-column>
-        </el-table>
+        <div v-if="isPictureUploadShow">
+          <el-upload
+            :action="url"
+            multiple
+            :limit="5"
+            list-type="picture-card"
+            :file-list="fileList"
+            :on-preview="handlePictureCardPreview"
+            :on-remove="handlePictureRemove"
+            :on-success="handlePictureSuccess"
+            :on-error="handlePictureError"
+            :on-exceed="handlePictureExceed"
+            :before-upload="beforePictureUpload">
+            <i class="el-icon-plus"></i>
+          </el-upload>
+          <el-dialog :visible.sync="previewDialogVisible" append-to-body>
+            <img width="100%" :src="previewDialogImageUrl" alt="">
+          </el-dialog>
+          <div style="margin-bottom: 10px"><label style="color: gray">注意！图片顺序与列表顺序一致！</label></div>
+          <el-table
+            :data="fileList"
+            border
+            v-loading="dataListLoading"
+            style="width: 100%;">
+            <el-table-column
+              prop="id"
+              header-align="center"
+              align="center"
+              width="80"
+              label="ID">
+            </el-table-column>
+            <el-table-column
+              prop="name"
+              header-align="center"
+              align="center"
+              label="图片名称">
+            </el-table-column>
+            <el-table-column
+              prop="createTime"
+              header-align="center"
+              align="center"
+              label="上传时间">
+            </el-table-column>
+          </el-table>
+        </div>
       </el-tab-pane>
       <el-tab-pane label="视频上传" name="videoUpload">
-        视频上传
+        <div v-if="isVideoUploadShow">
+          <el-upload
+            :action="url"
+            multiple
+            :limit="5"
+            list-type="picture-card"
+            :file-list="fileList"
+            :before-upload="beforeVideoUpload"
+            :on-exceed="handleVideoExceed"
+            :on-success="handleVideoSuccess"
+            :on-error="handleVideoError"
+            :on-remove="handleVideoRemove"
+            :on-preview="handleVideoCardPreview">
+            <i class="el-icon-plus"></i>
+          </el-upload>
+        </div>
       </el-tab-pane>
     </el-tabs>
     <span slot="footer" class="dialog-footer">
@@ -71,6 +82,7 @@
     data () {
       return {
         url: '',
+        activeName: 'pictureUpload',
         visible: false,
         dataListLoading: false,
         pageIndex: 1,
@@ -80,19 +92,23 @@
         successNum: 0,
         bdOrgId: 0,
         teacherId: 0,
+        typeId: 0,
+        fileList: [],
+        // 用于控制tab页的渲染
+        isPictureUploadShow: true,
+        isVideoUploadShow: false,
         // 以下是图片相关的参数
-        activeName: 'pictureUpload',
         previewDialogImageUrl: '',
-        previewDialogVisible: false,
-        pictureFileList: []
+        previewDialogVisible: false
       }
     },
     methods: {
-      init (bdOrgId, teacherId) {
+      init (bdOrgId, teacherId, typeId) {
         this.visible = true
         this.bdOrgId = bdOrgId
         this.teacherId = teacherId
-        this.url = this.$http.adornUrl(`/business/teachermultimedia/upload?token=${this.$cookie.get('token')}&teacherId=${teacherId}&bdOrgId=${bdOrgId}&typeId=${1}`)
+        this.typeId = typeId
+        this.url = this.$http.adornUrl(`/business/teachermultimedia/upload?token=${this.$cookie.get('token')}&teacherId=${teacherId}&bdOrgId=${bdOrgId}&typeId=${typeId}`)
         this.dataListLoading = true
         this.$http({
           url: this.$http.adornUrl('/business/teachermultimedia/list'),
@@ -102,14 +118,14 @@
             'limit': this.pageSize,
             'teacherId': teacherId,
             'bdOrgId': this.$store.state.user.id === 1 ? null : bdOrgId, // 超级管理员可以获取全部机构部门的列表
-            'typeId': 1 // 只查看图片的信息
+            'typeId': typeId // 只查看图片的信息
           })
         }).then(({data}) => {
           if (data && data.code === 0) {
-            this.pictureFileList = data.page.list
+            this.fileList = data.page.list
             this.totalPage = data.page.totalCount
           } else {
-            this.pictureFileList = []
+            this.fileList = []
             this.totalPage = 0
           }
           this.dataListLoading = false
@@ -117,12 +133,28 @@
 
         // 组件看不见时调用，清空数组
         this.over = () => {
-          this.pictureFileList = []
+          this.fileList = []
+          this.activeName = "pictureUpload"
+          this.isPictureUploadShow = true
+          this.isVideoUploadShow = false
         }
       },
       handleTabClick (tab, event) {
-        console.log(tab, event)
+        if (tab.name === "pictureUpload"){
+          this.isPictureUploadShow = true
+          this.isVideoUploadShow = false
+          this.num = 0
+          this.successNum = 0
+          this.init(this.bdOrgId, this.teacherId, 1)
+        } else if (tab.name === "videoUpload") {
+          this.isPictureUploadShow = false
+          this.isVideoUploadShow = true
+          this.num = 0
+          this.successNum = 0
+          this.init(this.bdOrgId, this.teacherId, 2)
+        }
       },
+      // 图片上传的相关事件
       handlePictureRemove (file, fileList) {
         if (file.status === 'success') {
           console.log(file, fileList)
@@ -131,7 +163,7 @@
             method: 'post',
             params: this.$http.adornParams({
               'id': file.id,
-              'name': file.name,
+              'objectName': file.objectName,
               'bdOrgId': this.bdOrgId
             })
           }).then(({data}) => {
@@ -141,7 +173,7 @@
                 type: 'success',
                 duration: 1500
               })
-              this.init(this.bdOrgId, this.teacherId) // 删除成功，刷新列表
+              this.init(this.bdOrgId, this.teacherId, 1) // 删除成功，刷新列表
             } else {
               this.$message.error(data.msg)
             }
@@ -167,7 +199,6 @@
 
         return isJPG && isLt2M
       },
-      // 上传成功
       handlePictureSuccess (response, file, fileList) {
         this.fileList = fileList
         this.successNum++
@@ -178,7 +209,7 @@
               cancelButtonText: '取消',
               type: 'warning'
             }).then(() => {
-              this.init(this.bdOrgId, this.teacherId) // 点击确定后，刷新列表
+              this.init(this.bdOrgId, this.teacherId, 1) // 点击确定后，刷新列表
             }).catch(() => {
               this.visible = false
             })
@@ -193,6 +224,77 @@
       handlePictureExceed (files, fileList) {
         this.$message.warning('上传不能超过5个！')
       },
+      // 视频上传的相关事件
+      beforeVideoUpload (file) {
+        const isVideo = ['video.mp4', 'video/flv', 'video/avi', 'video/rmvb'].indexOf(file.type) >= 0
+        const isLt10M = file.size / 1024 / 1024 < 10
+
+        if (!isVideo) {
+          this.$message.error('上传视频只能是 mp4、flv、avi、rmvb 等格式!')
+        }
+        if (!isLt10M) {
+          this.$message.error('上传视频大小不能超过 10MB!')
+        }
+
+        this.num++
+
+        return isVideo && isLt10M
+      },
+      handleVideoExceed (files, fileList) {
+        this.$message.warning('上传不能超过5个！')
+      },
+      handleVideoSuccess (response, file, fileList) {
+        this.fileList = fileList
+        this.successNum++
+        if (response && response.code === 0) {
+          if (this.num === this.successNum) {
+            this.$confirm('操作成功, 是否继续操作?', '提示', {
+              confirmButtonText: '确定',
+              cancelButtonText: '取消',
+              type: 'warning'
+            }).then(() => {
+              this.init(this.bdOrgId, this.teacherId, 2) // 点击确定后，刷新列表
+            }).catch(() => {
+              this.visible = false
+            })
+          }
+        } else {
+          this.$message.error(response.msg)
+        }
+      },
+      handleVideoError (err, file, fileList) {
+        this.$message.error(err)
+      },
+      handleVideoRemove (file, fileList) {
+        if (file.status === 'success') {
+          console.log(file, fileList)
+          this.$http({
+            url: this.$http.adornUrl('/business/teachermultimedia/deleteFromOss'),
+            method: 'post',
+            params: this.$http.adornParams({
+              'id': file.id,
+              'objectName': file.objectName,
+              'bdOrgId': this.bdOrgId
+            })
+          }).then(({data}) => {
+            if (data && data.code === 0) {
+              this.$message({
+                message: '成功删除',
+                type: 'success',
+                duration: 1500
+              })
+              this.init(this.bdOrgId, this.teacherId, 2) // 删除成功，刷新列表
+            } else {
+              this.$message.error(data.msg)
+            }
+          })
+        }
+      },
+      handleVideoCardPreview (file) {
+        // this.previewDialogImageUrl = file.url
+        // this.previewDialogVisible = true
+      },
+      // 关闭时的逻辑
       closeDialog () {
         this.over()
       }
