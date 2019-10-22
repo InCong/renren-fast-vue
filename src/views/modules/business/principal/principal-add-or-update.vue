be74ba7075cf53c22840c1cf055cb714f8b9cbb6
InCong <371605615@qq.com>
2019-10-22 17:49:37 +0800

    负责人信息增删查改与微信绑定

---
+++ src/views/modules/business/principal/principal-add-or-update.vue
@@ -0,0 +1,98 @@
+<template>
+  <el-dialog
+    :title="!dataForm.id ? '新增' : '修改'"
+    :close-on-click-modal="false"
+    :visible.sync="visible">
+    <el-form :model="dataForm" :rules="dataRule" ref="dataForm" @keyup.enter.native="dataFormSubmit()" label-width="80px">
+    <el-form-item label="名称" prop="name">
+      <el-input v-model="dataForm.name" placeholder="名称"></el-input>
+    </el-form-item>
+    <el-form-item label="联系电话" prop="mobile">
+      <el-input v-model="dataForm.mobile" placeholder="联系电话"></el-input>
+    </el-form-item>
+    <el-form-item label="创建时间" prop="createTime">
+      <el-input v-model="dataForm.createTime" placeholder="创建时间，自动生成，无需填写" :disabled="true"></el-input>
+    </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="visible = false">取消</el-button>
+      <el-button type="primary" @click="dataFormSubmit()">确定</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        visible: false,
+        dataForm: {
+          id: 0,
+          name: '',
+          mobile: ''
+        },
+        dataRule: {
+          name: [
+            { required: true, message: '名称不能为空', trigger: 'blur' }
+          ],
+          mobile: [
+            { required: true, message: '联系电话不能为空', trigger: 'blur' }
+          ]
+        }
+      }
+    },
+    methods: {
+      init (id) {
+        this.dataForm.id = id || 0
+        this.visible = true
+        this.$nextTick(() => {
+          this.$refs['dataForm'].resetFields()
+          if (this.dataForm.id) {
+            this.$http({
+              url: this.$http.adornUrl(`/business/principal/info/${this.dataForm.id}`),
+              method: 'get',
+              params: this.$http.adornParams()
+            }).then(({data}) => {
+              if (data && data.code === 0) {
+                this.dataForm.name = data.principal.name
+                this.dataForm.mobile = data.principal.mobile
+                this.dataForm.createTime = data.principal.createTime
+              }
+            })
+          }
+        })
+      },
+      // 表单提交
+      dataFormSubmit () {
+        this.$refs['dataForm'].validate((valid) => {
+          if (valid) {
+            this.$http({
+              url: this.$http.adornUrl(`/business/principal/${!this.dataForm.id ? 'save' : 'update'}`),
+              method: 'post',
+              data: this.$http.adornData({
+                'id': this.dataForm.id || undefined,
+                'name': this.dataForm.name,
+                'mobile': this.dataForm.mobile,
+                'bdOrgId': this.dataForm.bdOrgId || this.$store.state.user.bdOrgId
+              })
+            }).then(({data}) => {
+              if (data && data.code === 0) {
+                this.$message({
+                  message: '操作成功',
+                  type: 'success',
+                  duration: 1500,
+                  onClose: () => {
+                    this.visible = false
+                    this.$emit('refreshDataList')
+                  }
+                })
+              } else {
+                this.$message.error(data.msg)
+              }
+            })
+          }
+        })
+      }
+    }
+  }
+</script>
